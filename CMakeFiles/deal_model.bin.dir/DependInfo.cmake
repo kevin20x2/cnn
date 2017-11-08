a47762ab306cff9d89ae12702fe5d3b7f69c4562
kevin20x2 <kevin20x2@hust.edu.cn>
2017-11-08 21:37:55 +0800

    first commit

---
+++ CMakeFiles/deal_model.bin.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/deal_model/deal_model.cpp" "/home/kevin/deal_model/CMakeFiles/deal_model.bin.dir/deal_model.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/OpenBLAS/include"
+  "/home/kevin/caffe-master/include"
+  "/home/kevin/caffe-master/build/src"
+  "/home/kevin/caffe-master/build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ deal_model.cpp
@@ -0,0 +1,65 @@
+#define CPU_ONLY
+#include "caffe/caffe.hpp"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace caffe;
+//#undef NDEBUG
+//char * proto = "/home/ftp/VGG_ILSVRC_19_layers_deploy.prototxt";
+char * proto = "VGG_ILSVRC_19_layers_deploy.prototxt";
+char * model = "VGG_ILSVRC_16_layers.caffemodel";
+Phase phase = TEST;
+int main()
+{
+
+	Caffe::set_mode(Caffe::CPU);
+	boost::shared_ptr < Net<float> > net(new caffe::Net<float>(proto,phase));
+	//net->CopyTrainedLayersFrom(model);
+	NetParameter param;
+	ReadNetParamsFromBinaryFileOrDie(model,&param);
+    param.mutable_state()->set_phase(TEST);
+    param.mutable_state()->set_level(0);
+   // param
+    //boost::shared_ptr < Net<float> > net(new caffe::Net<float>(param));
+    //net->CopyTrainedLayersFrom(param);
+    //net->Init(param);
+	int num_layers = param.layer_size();
+
+    auto layer_list = net->layers();
+
+    for(int i =0;i<num_layers;++i)
+    {
+        DLOG(ERROR)<<"Layer" <<i<<":" <<param.layer(i).name()<<"\t"
+            << param.layer(i).type();
+        if(param.layer(i).type() == "Convolution")
+        {
+            ConvolutionParameter conv_param = param.layer(i).convolution_param();
+            LOG(ERROR)<<"kernel size number"<<conv_param.kernel_size().size();
+            int blob_num = param.layer(i).blobs_size();
+            LOG(ERROR)<<"blobs size " << blob_num;
+            layer_list[i]->blobs().resize(blob_num);
+            for(int j = 0;j<blob_num;++j)
+            {
+                Blob <float > * weights;
+                //if( layer_list[i]->blobs().size()>0) {
+                  //  weights = layer_list[i]->blobs()[0].get();
+                layer_list[i]->blobs()[j].reset( new Blob<float>());
+                layer_list[i]->blobs()[j]->FromProto(param.layer(i).blobs(j));
+                weights = layer_list[i]->blobs()[j].get();
+
+                LOG(ERROR) <<"shape :" <<weights->shape(0)
+                           <<" " << weights->shape(1)
+                        <<" " << weights->shape(2)
+                        <<" " << weights->shape(3);
+               // }
+
+                LOG(ERROR)<<"blob data size:"<<j<<" "<<param.layer(i).blobs(j).data().size();
+            }
+
+        }
+    }
+
+    return 0;
+}
